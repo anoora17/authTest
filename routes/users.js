f0ff63347f85cb5f7a466cc43399c4e9468440f2
Your Name <anoor@witweel.com>
2017-08-25 10:01:55 -0400

    index

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log(req.user)
-  res.render({'user':{all: req.user
-                      }
-        });
+router.use('/', function(req, res, next){
+
+    if(!req.user){
+        res.redirect('/');
+    }
+    next();
+})
+/* GET users listing. */
+router.get('/', function(req, res) {
+  res.render('signup', {user: {name: req.user.displayName,
+                              image: req.user.image}});
 });
 
 module.exports = router;
